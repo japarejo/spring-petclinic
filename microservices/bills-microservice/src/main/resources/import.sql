f83deb700dfd9201329c9a58bc453870a00a505d
José Antonio Parejo Maestre <japarejo@gmail.com>
2021-06-08 15:37:51 +0200

    Microservice working

--- microservices/bills-microservice/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO bill(id,version,visit_id,amount,concept) VALUES(1,1,1,39.99,'Revisión rutinaria');
-INSERT INTO bill(id,version,visit_id,amount,concept) VALUES(2,1,2,59.99,'Desparasitación');
