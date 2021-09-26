ae47484653700d3122a4fa8dba7e9353a3b23423
José Antonio Parejo Maestre <japarejo@gmail.com>
2021-09-26 18:20:42 +0200

    First version working, loading from a Blog feed and creating a new entry in a file (with a transformation between(

--- src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetclinicApplication.java
@@ -1,14 +1,23 @@
 package org.springframework.samples.petclinic;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication()
+@ImportResource("/integration/integration.xml")
 public class PetclinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetclinicApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		//SpringApplication.run(PetclinicApplication.class, args);
+		ConfigurableApplicationContext ctx = new SpringApplication(PetclinicApplication.class).run(args);
+	    System.out.println("Hit Enter to terminate");
+	    System.in.read();
+	    ctx.close();
 	}
 
 }
