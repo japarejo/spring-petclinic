59b81936ec4e58c3f9907d4f480fc87b5cda12b1
José Antonio Parejo Maestre <japarejo@gmail.com>
2021-11-22 16:41:54 +0100

    Merge branch 'master'
    Conflicts:
    	pom.xml

--- src/test/java/org/springframework/samples/petclinic/service/PetServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/PetServiceTests.java
@@ -26,7 +26,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.omg.PortableInterceptor.ORBInitInfoPackage.DuplicateName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
